f43d629c2eda8518fef4a9b590815ced0e2a0872
LFTKazu <kas.bell00@gmail.com>
2021-07-22 23:41:44 +0900

    change all component color.

--- pages/theme.ts
+++ pages/theme.ts
@@ -1,21 +1,16 @@
-import { createMuiTheme } from '@material-ui/core/styles'
-import { red } from '@material-ui/core/colors'
+//この画面でcomponentのcolorが変更できる
+import { createTheme } from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple';
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
-    //primary: {
-    //  main: '#556cd6',
-    //},
-    //secondary: {
-    //  main: '#19857b',
-    //},
-    error: {
-      main: red.A400,
+    primary: {
+      main: purple[500],
     },
-    background: {
-      default: '#fff',
+    secondary: {
+      main: '#ea80fc',
     },
   },
-})
+});
 
 export default theme
