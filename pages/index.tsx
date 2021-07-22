7d5e41e0495936d630f88459237a3c4d15155375
LFTKazu <kas.bell00@gmail.com>
2021-07-22 23:44:39 +0900

    delete indention

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Link from 'next/link';
 
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
